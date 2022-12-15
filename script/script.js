40b8f6bfb94fdc9d86233187441a3239af09b4d1
nika_chorna <veronika.chorna2000@gmail.com>
2022-12-15 22:57:09 +0200

    resolve a few tasks

--- script/script.js
+++ script/script.js
@@ -1,20 +1,12 @@
 //  1.Створити функцію getMaxDigit(number) – яка отримує будь-яке число та
 // виводить найбільшу цифру в цьому числі. Приклади: 1236 -> 6, 987 -> 9, 385 -> 8
 
-// const num = 45654356;
-// const greatestDigit = (num = 0, greatest = 0) => {
-//    if(num){
-//       const max = Math.max(num % 10, greatest);
-//       return greatestDigit(Math.floor(num / 10), max);
-//    };
-//    return greatest;
-// };
-// console.log(greatestDigit(num));
+
 
 // 2.Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та
 // **. Використовуйте цикл
 
-function numberDegree(x, n) {
+function pow(x, n) {
     let result = x;
   
     for (let i = 1; i < n; i++) {
@@ -23,37 +15,121 @@ function numberDegree(x, n) {
   
     return result;
   }
-  console.log(`Function result: ${numberDegree(3, 3)}`);
+  console.log(`Function determines the power of a number: ${pow(3, 3)}`);
 
 
 // 3.Створити функцію, яка форматує ім'я, роблячи першу букву великою. ("влад" ->
 // "Влад", "вЛАД" -> "Влад" і так далі);
 
-function firstLetterUpperCase(string) {
+function getfirstLetterUpperCase(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-console.log(`Function result: ${firstLetterUpperCase('cHARacTer')}`);
+console.log(`Function formats the name by capitalizing the first letter: ${getfirstLetterUpperCase('cHARacTer')}`);
 
 // 4.Створити функцію, яка вираховує суму, що залишається після оплати податку від
 // зарабітньої плати. (Податок = 18% + 1.5% -> 19.5%). Приклад: 1000 -> 805
 
-function salaryAfterTax(a) {
-    let result = a - ((a / 100 * 18) + (a / 100 * 1.5));
+function getsalaryAfterTax(a) {
+    let result = a - ((a / 100 * 19.5));
     return result;
 }
 
-console.log(`Function result: ${salaryAfterTax(2000)}`);
+console.log(`Function deducts the amount remaining after paying the tax: ${getsalaryAfterTax(2000)}`);
 
 // 5.Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M.
 // Приклад: getRandomNumber(1, 10) -> 5
 
-function getRandomNumber(a, b) {
-    return Math.random(a, b);
+function getRandomNumber(m, n) {
+    return (Math.random() * (m - n + 1) + n).toFixed(0);
+    
 }
 
-console.log(`Function result: ${getRandomNumber(10, 50)}`);
+
+console.log(`Function return a random integer in the range N to M: ${getRandomNumber(1, 50)}`);
 
 
 // 6.Створити функцію, яка рахує скільки разів певна буква повторюється в слові.
 // Приклад: countLetter("а", "Асталавіста") -> 4
+
+function getcountLetter(str, a) {
+
+let value = 0;
+for (i = 0; i < str.length; i++) {
+  if (str.charAt(i) == a) {
+    let i = 1;
+    value += i;
+  }
+}
+ return value;
+}
+
+console.log(`Function return how many times a certain letter is repeated in a word: ${getcountLetter('blablablablabla', 'a')}`);
+
+
+// 7.Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від
+// наявності символа $ або UAH в рядку. Приклад: convertCurrency("100$") -> 2500
+// грн. або convertCurrency("2500UAH") -> 100$
+// Врахуйте, інші валюти не конвертуються, потрібно виводити помилку, і також
+// регістр uah не має значення.
+
+// 8.Створіть функцію генерації випадкового паролю (тільки числа), довжина
+// встановлюється користувачем або по замовчуванню = 8 символам.
+// Приклад: getRandomPassword(4) -> 1875, getRandomPassword() -> 87240124
+
+
+function getRandomPassword(n) {
+  let pwd = '';
+  while(n > 0) {
+    pwd += Math.floor(Math.random() * 10);
+    n--;
+  }
+  return pwd; 
+}
+
+console.log(`Function that return random password: ${getRandomPassword(8)}`);
+
+// 9.Створіть функцію, яка видаляє всі букви з речення. Приклад: deleteLetters('a',
+// "blablabla") -> "blblbl"
+
+function deleteLetters(a, str) {
+  let value = '';
+  for (i = 0; i < str.length; i++) {
+    if (str.charAt(i) !== a) {
+      value += str.charAt(i);
+    }
+  }
+   return value;
+}
+
+console.log(`Function deletes selected letters: ${deleteLetters('a', 'It was realy beautiful day')}`);
+
+// 10.Створіть функцію, яка перевіряє, чи є слово паліндромом. Приклад:
+// isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу
+// гусеня") -> true
+
+function isPalyndrom(str) {
+  value = '';
+  for (i = str.length - 1; i >= 0; i--) {
+    value += str.charAt(i);
+  }
+
+  return (str === value) ? true : false;
+}
+
+console.log(`Function checks if word is palidrom: ${isPalyndrom('something')}`);
+
+// 11.Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу.
+// Приклад: deleteDuplicateLetter("Бісквіт був дуже ніжним") -> "сктдеим"
+
+function deleteDuplicateLetter(a, str) {
+  let value = '';
+  for (i = 0; i < str.length; i++) {
+    if (str.charAt(i)) {
+      value += str.charAt(i);
+    }
+  }
+   return value;
+}
+
+console.log(`Function deletes duplicated letters: ${deleteDuplicateLetter('a', 'It was realy beautiful day')}`);
