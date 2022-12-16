234a3ef2401aac64f204d64c31c5c60516a4a7cf
nika_chorna <veronika.chorna2000@gmail.com>
2022-12-16 18:44:28 +0200

    add function 10

--- script/js.js
+++ script/js.js
@@ -2,11 +2,9 @@
 // 2.Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл
 function getNumberPow(x, n) {
     let result = x;
-  
     for (let i = 1; i < n; i++) {
       result *= x;
     }
-  
     return result;
   }
 
@@ -33,7 +31,6 @@ console.log(`Function deducts the amount remaining after paying the tax: ${getsa
 // 5.Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M. Приклад: getRandomNumber(1, 10) -> 5
 function getRandomNumber(m, n) {
     return (Math.random() * (m - n + 1) + n).toFixed(0);
-    
 }
 
 console.log(`Function return a random integer in the range N to M: ${getRandomNumber(1, 50)}`);
@@ -41,14 +38,13 @@ console.log(`Function return a random integer in the range N to M: ${getRandomNu
 // ---------------------------------------------------------------------------------------------------------------------------
 // 6.Створити функцію, яка рахує скільки разів певна буква повторюється в слові. Приклад: countLetter("а", "Асталавіста") -> 4
 function getcountLetter(str, a) {
-
-let value = 0;
-for (i = 0; i < str.length; i++) {
-  if (str.charAt(i).toLowerCase() == a) {
-    value += 1;
-  }
-}
- return value;
+    let value = 0;
+    for (i = 0; i < str.length; i++) {
+        if (str.charAt(i).toLowerCase() == a) {
+            value += 1;
+        }
+    }
+    return value;
 }
 
 console.log(`Function return how many times a certain letter is repeated in a word: ${getcountLetter('blablablablablablablA', 'a')}`);
@@ -59,8 +55,8 @@ console.log(`Function return how many times a certain letter is repeated in a wo
 function getRandomPassword(n = '8') {
     let pwd = '';
     while(n > 0) {
-      pwd += Math.floor(Math.random() * 10);
-      n--;
+        pwd += Math.floor(Math.random() * 10);
+        n--;
     }
     return pwd; 
   }
@@ -69,15 +65,35 @@ function getRandomPassword(n = '8') {
 
 // ---------------------------------------------------------------------------------------------------------
 // 9.Створіть функцію, яка видаляє всі букви з речення. Приклад: deleteLetters('a', "blablabla") -> "blblbl"
-
 function deleteLetters(a, str) {
     let value = '';
     for (i = 0; i < str.length; i++) {
-      if (str.charAt(i).toLowerCase() !== a) {
+        if (str.charAt(i).toLowerCase() !== a) {
         value += str.charAt(i);
-      }
+        }
     }
-     return value;
+    return value;
   }
   
   console.log(`Function deletes selected letters: ${deleteLetters('a', 'It was realy beautiful day')}`);
+
+// --------------------------------------------------------------------------------------------------------------
+// 10.Створіть функцію, яка перевіряє, чи є слово паліндромом. Приклад: isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу гусеня") -> true
+function isPalyndrom(str) {
+    str = str.toLowerCase();
+    strNew = '';
+    value = '';
+    for (i = 0; i < str.length; i++) {
+      if (str.charAt(i) !== ' ') {
+        strNew += str.charAt(i);
+      }
+    }
+    for (i = strNew.length - 1; i >= 0; i--) {
+      if (strNew.charAt(i) !== ' ') {
+        value += strNew.charAt(i);
+      }
+  }
+    return (strNew === value) ? true : false;
+  }
+  
+  console.log(`Function checks if word is palidrom: ${isPalyndrom('Madam madam')}`);
